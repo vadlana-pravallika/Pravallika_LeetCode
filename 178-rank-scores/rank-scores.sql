1aff5aa9ac7dc722d20fae33306a8fb0f8feea1a
Vadlana Pravallika <vadlanapravallika@gmail.com>
2026-02-28 15:18:36 +0530

    Time: 352 ms (80.99%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,4 +1,4 @@
 /* Write your PL/SQL query statement below */
 
 
-select score , dense_rank() over (order by score desc) as rank from scores order by score desc;
+select score , dense_rank() over (order by score desc) as rank from scores;
