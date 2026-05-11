1b05d99b91d9f006ef5054d652176c6630095e96
Vadlana Pravallika <vadlanapravallika@gmail.com>
2026-05-11 15:12:32 +0530

    Time: 1203 ms (5.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,5 @@
+SELECT name,COALESCE(SUM(distance),0) AS travelled_distance
+FROM Users U 
+LEFT JOIN rides R ON U.id=R.user_id 
+GROUP BY R.user_id, name
+ORDER BY travelled_distance DESC, name ASC
