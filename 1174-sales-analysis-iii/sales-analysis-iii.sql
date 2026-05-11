7bb39bc1c216bbf602a216a58fe91e475b59de9f
Vadlana Pravallika <vadlanapravallika@gmail.com>
2026-05-11 13:44:15 +0530

    Time: 965 ms (52.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+select product_id,product_name from product
+where product_id in (select product_id from sales where sale_date between '2019-01-01' and '2019-03-31') 
+and product_id not  in (select product_id from sales where sale_date not between '2019-01-01' and '2019-03-31')
